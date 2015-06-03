42af939c8dca3ff861dce8ebf78f18a0dc0827d0
otabari <otabari@users.noreply.github.com>
2015-06-03 11:52:16 +0200

    Update rpmbuilder.sh
    added "gcc" to the list of required packages

--- rpmbuilder.sh
+++ rpmbuilder.sh
@@ -7,7 +7,7 @@ RED='\033[0;31m'
 DEFCOLOR='\033[0m'
 GREEN='\e[0;32m'
 
-packages="gcc wget pcre pcre-devel socat openssl-devel openssl rpm-build mock"
+packages="gcc wget pcre pcre-devel socat openssl-devel openssl rpm-build mock gcc"
 missing_pkgs=""
 
 display_usage() { 
